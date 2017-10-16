3173998148b68588efbe13a1a35297741c711296
Norvinder Singh <norvinder@gmail.com>
2017-10-16 19:15:06 -0400

    Added *ngFor Directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,9 +6,12 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent implements OnInit {
- recipes: Recipe[] = [];
+ recipes: Recipe[] = [
+   new Recipe('Schnitzel', 'Very Tasty', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg', []),
+   new Recipe ('Summer Salad', 'Okayish', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg' ,[]) 
+ ];
  @Output() recipeSelected = new EventEmitter<Recipe>();
- recipe = new Recipe ('Dummy', 'Dummy','http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg');
+
   constructor() { }
 
   ngOnInit() {
